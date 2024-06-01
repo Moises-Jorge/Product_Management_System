84d345e5ad4bf91e49831dbe678325875ed755d5
Moisés Jorge <moisesjorge30@gmail.com>
2024-06-01 12:40:27 +0100

    creation of  the project

---
+++ src/main/java/com/project_gemini_ai/sgpd/SgpdApplication.java
@@ -0,0 +1,13 @@
+package com.project_gemini_ai.sgpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpdApplication.class, args);
+	}
+
+}
